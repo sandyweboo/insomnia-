8779e1dcfc6f3d65ea0055a47457e6ef0ebc52be
sandyweboo <102416086+sandyweboo@users.noreply.github.com>
2024-02-14 07:17:51 +0530

    bcrypt

--- controller/authController.js
+++ controller/authController.js
@@ -2,15 +2,21 @@ import express  from "express";
 import User from "../model/userModel.js";
 import { errorHandler } from "../utils/error.js";
 
+import bcrypt from 'bcryptjs'
+
+
 export const signup = async(req, res, next)=>{
 const {username, email, password} = req.body;
 if(!username || !email || !password || username === '' || email === '' || password === ''){
     next (errorHandler(400, "all field are reqiure"))
 }
+
+const hash = bcrypt.hashSync(password, 8);
+console.log(hash)
 const newUser = new User({
     username,
     email,
-    password
+    password:hash
 }) 
 try {
     await newUser.save();
@@ -26,10 +32,15 @@ export const signin = async(req, res, next)=>{
         next (errorHandler(400, "all field are reqiuress"))
     }
 try {
+
+    
     const validUser = await User.findOne({email})
+  
     if (!validUser) {
         return next(errorHandler(401, "Invalid user"));
-    } else if (password !== validUser.password) {
+    } 
+    const validPassword = bcrypt.compareSync(password, validUser.password)
+    if (!validPassword) {
         return next(errorHandler(401, "Invalid password"));
     } else {
         // Authentication successful, you may want to generate and send a token here
